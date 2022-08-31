10c3b05535b24ccdf6f4e758dcf175e16892156d
Ilya <koselevicila@gmail.com>
2022-08-31 16:59:06 +0300

    ignat-1

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -1,19 +1,21 @@
 import React from 'react'
-
+import {AffairType} from "./HW2";
+import s from './Affairs.module.css'
 type AffairPropsType = {
     // key не нужно типизировать
-    affair: any // need to fix any
-    deleteAffairCallback: any // need to fix any
+    affair:AffairType // need to fix any
+    deleteAffairCallback: (id:number)=>void // need to fix any
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {}// need to fix
-
+    const deleteCallback = () => {props.deleteAffairCallback(props.affair._id)}// need to fix
+    const prioriteClass = s.iten + '  ' + s[props.affair.priority]
     return (
-        <div>
-            // show some text
+        <div className={s.affair}>
+            <div className={s.iten}>{props.affair.name}</div>
+            <div className={prioriteClass}>{props.affair.priority}</div>
 
-            <button onClick={deleteCallback}>X</button>
+            <button onClick={deleteCallback} className={s.iten + '' + s.button}>X</button>
         </div>
     )
 }
--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,36 +1,64 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import {AffairType, FilterType} from './HW2'
+import s from './Affairs.module.css'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+    data: AffairType[]
+    setFilter: (filter: FilterType) => void
+    deleteAffairCallback: (id: number) => void
+    filter: FilterType
+
 }
 
 function Affairs(props: AffairsPropsType) {
     const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
+        <Affair
+            key={a._id}
             affair={a}
             deleteAffairCallback={props.deleteAffairCallback}
         />
     ))
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+    const setAll = () => {
+        props.setFilter('all')
+    } // need to fix
+    const setHigh = () => {
+        props.setFilter('high')
+    }
+    const setMiddle = () => {
+        props.setFilter('middle')
+    }
+    const setLow = () => {
+        props.setFilter('low')
+    }
+    const set = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+        props.setFilter(e.currentTarget.value as FilterType)
+    }
+
+    const cnAll = s.button + '  ' + (props.filter === 'all' ? s.active : '')
+    const cnHing = s.button + '  ' + (props.filter === 'high' ? s.active : '')
+    const cnMiddle = s.button + '  ' + (props.filter === 'middle' ? s.active : '')
+    const cnLow = s.button + '  ' + (props.filter === 'low' ? s.active : '')
+    const setClass = (filter: FilterType) => {
+        return s.button + (props.filter === filter ? '  ' + s.active : '')
+    }
 
     return (
         <div>
 
             {mappedAffairs}
 
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
+            <button onClick={setAll} className={cnAll}>All</button>
+            <button onClick={setHigh} className={cnHing}>High</button>
+            <button onClick={setMiddle} className={cnMiddle}>Middle</button>
+            <button onClick={setLow} className={cnLow}>Low</button>
+
+
+            <button onClick={set} className={setClass('all')} value={'all'}>All</button>
+            <button onClick={set} className={setClass('high')} value={'high'}>High</button>
+            <button onClick={set} className={setClass('middle')} value={'middle'}>Middle</button>
+            <button onClick={set} className={setClass('low')} value={'low'}>Low</button>
         </div>
     )
 }
