c6d9dce3149416ed9b73ac880a46fa295e19732f
Ilya <koselevicila@gmail.com>
2022-09-20 17:41:54 +0300

    h5

--- src/p2-homeworks/h5-rrd-v6/Header.tsx
+++ src/p2-homeworks/h5-rrd-v6/Header.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import s from './Header.module.css'
+import {NavLink} from "react-router-dom";
 
 function Header() {
     return (
-        <div>
-            // add NavLinks
-
+        <div className={s.header}>
+            <div>
+                <NavLink to={'/'} className={s.navLink}>Prejonior</NavLink>
+                <NavLink to={'/junior'} className={s.navLink}> Junior</NavLink>
+                <NavLink to={'/junior+'} className={s.navLink}> Junior+</NavLink>
+            </div>
+            <div className={s.b}></div>
         </div>
     )
 }
