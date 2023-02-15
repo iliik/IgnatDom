c7f07449e65fc1208491431670e4086bc0132fec
Ilya <koselevicila@gmail.com>
2023-02-15 19:06:15 +0300

    № 5-CSS-Sidebar

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -31,7 +31,9 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         id={'hw5-pre-junior-link'}
                         to={PATH.PRE_JUNIOR}
                         onClick={handleClose}
+
                         // className={...} // делает студент
+                        className={({isActive}) => isActive ? s.active : ''}
                     >
                         Pre-junior
                     </NavLink>
@@ -40,6 +42,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
                         to={PATH.JUNIOR}
                         onClick={handleClose}
                         // className={...} // делает студент
+                        className={({isActive}) => isActive ? s.active : ''}
                     >
                         Junior
                     </NavLink>
