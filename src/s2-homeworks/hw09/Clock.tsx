b0918a6bc5bca02a2205a735203e8883c3b7b07f
Ilya <koselevicila@gmail.com>
2023-03-06 15:02:48 +0300

    HW9-start

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -12,7 +12,8 @@ function Clock() {
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-
+        let time = window.setInterval(() => setDate(new Date()), 1000)
+        setTimerId(time)
     }
 
     const stop = () => {
